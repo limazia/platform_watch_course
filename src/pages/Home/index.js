cc6a53d94feefb9748e11ca5be259fb51f1c02dd
Acacio de Lima <32038004+limazia@users.noreply.github.com>
2022-06-01 20:25:42 -0300

    Update index.js

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -14,7 +14,7 @@ function Home() {
                   <div className="card-body">
                     <iframe
                       className="w-100"
-                      src="https://www.youtube.com/embed/MXzGot5bcYw"
+                      src="https://www.youtube.com/embed/0dCrA5_KZDA"
                       frameborder="0"
                       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                       allowfullscreen=""
@@ -35,8 +35,8 @@ function Home() {
                 <div className="card card-list-classes">
                   <div className="card-body">
                     <div className="info">
-                      <small>Aula 01 - 30/05 às 19h</small>
-                      <p className="inactive">O começo de tudo</p>
+                      <small>Aula 01 - 00/00 às 19h</small>
+                      <p className="inactive">Título da aula!</p>
                     </div>
                     <div className="icon">
                       <i className="fas fa-play-circle play"></i>
@@ -50,8 +50,8 @@ function Home() {
                 <div className="card card-list-classes">
                   <div className="card-body">
                     <div className="info">
-                      <small>Aula 02 - 31/05 às 19h</small>
-                      <p>Algumas abstrações</p>
+                      <small>Aula 02 - 00/00 às 19h</small>
+                      <p>Título da aula!</p>
                     </div>
                     <div className="icon">
                       <i className="fas fa-signal-stream active"></i>
@@ -66,8 +66,8 @@ function Home() {
                   <div className="card-body">
                     <div className="info">
                       <span className="badge badge-soon">Em breve</span>
-                      <small className="soon">Aula 03 - 01/06 às 19h</small>
-                      <p className="soon">O coração do projeto</p>
+                      <small className="soon">Aula 03 - 00/00 às 19h</small>
+                      <p className="soon">Título da aula!</p>
                     </div>
                     <div className="icon">
                       <i className="fas fa-clock soon"></i>
@@ -82,8 +82,8 @@ function Home() {
                   <div className="card-body">
                     <div className="info">
                       <span className="badge badge-soon">Em breve</span>
-                      <small className="soon">Aula 04 - 02/06 às 19h</small>
-                      <p className="soon">Tudo funcionando</p>
+                      <small className="soon">Aula 04 - 00/00 às 19h</small>
+                      <p className="soon">Título da aula!</p>
                     </div>
                     <div className="icon">
                       <i className="fas fa-clock soon"></i>
@@ -98,8 +98,8 @@ function Home() {
                   <div className="card-body">
                     <div className="info">
                       <span className="badge badge-soon">Em breve</span>
-                      <small className="soon">Aula 05 - 03/06 às 19h</small>
-                      <p className="soon">Mostre ao mundo!</p>
+                      <small className="soon">Aula 05 - 00/00 às 19h</small>
+                      <p className="soon">Título da aula!</p>
                     </div>
                     <div className="icon">
                       <i className="fas fa-clock soon"></i>
